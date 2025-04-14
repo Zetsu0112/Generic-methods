39468fc88bfbf35b06e9389f44d760cc27ce295f
Philnator <phdj0112@gmail.com>
2025-04-14 19:45:53 +0200

    added "sum" method

--- src/GenericUtils.java
+++ src/GenericUtils.java
@@ -10,4 +10,12 @@ public class GenericUtils {
             lastIndex--;
         }
     }
+
+    public static <T extends Number> double sum (T[] array) {
+        double sum = 0.0;
+        for(int i = 0; i < array.length; i++) {
+            sum += array[i].doubleValue();
+        }
+        return sum;
+    }
 }
