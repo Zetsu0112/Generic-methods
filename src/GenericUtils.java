b3cca15a2ab2d22e2d2432af8ca38c0f72b69383
Philnator <phdj0112@gmail.com>
2025-04-14 19:48:45 +0200

    added javadoc comments to code

--- src/GenericUtils.java
+++ src/GenericUtils.java
@@ -1,6 +1,16 @@
-
+/**
+ * Provides generic methods for arrays of generic types
+ * @author Philip Djurak
+ * @version 13-04-2025
+ */
 public class GenericUtils {
 
+    /**
+     * Reverses elements of array
+     * 
+     * @param <T> the type of elements in the array
+     * @param array array that is reversed
+     */
     public static <T> void reverse (T[] array) {
         int lastIndex = array.length-1;
         for(int i = 0; i < array.length/2; i++) {
@@ -11,6 +21,13 @@ public class GenericUtils {
         }
     }
 
+    /**
+     * Returns sum of all elements of the array
+     * 
+     * @param <T> the type of number in the array
+     * @param array array of numbers
+     * @return the sum of all numbers from array as a double
+     */
     public static <T extends Number> double sum (T[] array) {
         double sum = 0.0;
         for(int i = 0; i < array.length; i++) {
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
-
+/**
+ * Tests Generic Methods From GenericUtils class
+ * @author Philip Djurak
+ * @version 13-04-2025
+ */
 
 public class Main {
     public static void main(String[] args) {
 
+        // Test reverse method (Swapping)
         Integer[] intNumbers = {1, 2, 3, 4, 5};
         GenericUtils.reverse(intNumbers);
 
@@ -10,6 +15,7 @@ public class Main {
             System.out.println(intNumbers[i]);
         }
 
+        // Test sum method
         Double[] doubleNumbers = {1.2, 3.4, 2.8};
         double sum = GenericUtils.sum(doubleNumbers);
         System.out.println(sum);
